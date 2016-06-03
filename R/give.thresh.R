de73106abb5297ebf3ed7aa0fc23627bd1847bb7
Stéphane Dray <stephane.dray@univ-lyon1.fr>
2016-06-03 15:27:50 +0200

    The function give.thresh now accepts distance matrix stored as matrices

--- R/give.thresh.R
+++ R/give.thresh.R
@@ -4,7 +4,7 @@
 #' It is used to select a truncation value for the dbMEM approach. It
 #' returns the minimum value that keep all samples connected.
 #' 
-#' @param distxy A distance matrix (class \code{dist})
+#' @param matdist A distance matrix (class \code{dist} or \code{matrix})
 #'   
 #' @return The maximum distance of the minimum spanning tree
 #' @author Stéphane Dray \email{stephane.dray@@univ-lyon1.fr}
@@ -17,7 +17,8 @@
 #' th <- give.thresh(dxy)
 
 "give.thresh" <-
-    function(distxy) {
-        spanning <- mstree(distxy)
-        return(max(neig2mat(spanning) * as.matrix(distxy)))
+    function(matdist) {
+        matdist <- as.dist(matdist)
+        spanning <- mstree(matdist)
+        return(max(neig2mat(spanning) * as.matrix(matdist)))
     }
