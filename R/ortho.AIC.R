65c49f2679414368039d76928c9621d666b2b514
Stéphane Dray <stephane.dray@univ-lyon1.fr>
2018-09-13 13:52:08 +0200

    Restore some functions used in the NewR book of Borcard et al.

---
+++ R/ortho.AIC.R
@@ -0,0 +1,101 @@
+#' Compute AIC for models with orthonormal explanatory variables
+#' 
+#' This function compute corrected AIC for models with orthonormal and centered 
+#' explanatory variables such as MEM spatial eigenfunctions. Variables are
+#' sorted by their contribution to R2.
+#' 
+#' It ensures that a model with k variables is the best one that can be 
+#' obtained. By default, response variables are centered (model with intercept).
+#' 
+#' 
+#' @param Y A matrix with response variables (univariate or multivariate 
+#'   response)
+#' @param X A set of orthonormal and centered vectors
+#' @param ord.var A logical value indicating if the order of variables and 
+#'   cumulative R2 must be returned
+#' @return A vector with corrected AIC if \code{ord.var=FALSE}. A list if
+#'   \code{ord.var=TRUE} with: \item{AICc }{Values of corrected AIC.}
+#'   \item{AICc0 }{Values of corrected AIC for the null model (only intercept).}
+#'   \item{ord }{Order of variables to be enter in the model} \item{R2
+#'   }{Cumulative R2}
+#' @author Stéphane Dray \email{stephane.dray@@univ-lyon1.fr}
+#' @references Godinez-Dominguez E. and Freire J. (2003) Information-theoretic 
+#'   approach for selection of spatial and temporal models of community 
+#'   organization. Marine Ecology - Progress Series. 253, 17--24
+#' @keywords models
+#' @examples
+#' 
+#' y <- matrix(rnorm(50),50,1)
+#' x <- svd(scale(y \%*\% c(0.1,0.5,2,0,0.7)+matrix(rnorm(250),50,5)))$u
+#' res <- ortho.AIC(y,x,ord.var=TRUE)
+#' minAIC <- which.min(res$AICc)
+#' nvar <- length(1:minAIC)+1 # number of orthogonal vectors + 1 for intercept
+#' lm1 <- lm(y~x[,res$ord[1:minAIC]])
+#' summary(lm1)$r.squared # R2
+#' res$R2[minAIC] # the same
+
+#' min(res$AICc) # corrected AIC
+#' extractAIC(lm1) # classical AIC
+#' min(res$AICc)-2*(nvar*(nvar+1))/(nrow(x)-nvar-1) # the same
+#'
+#' lm2 <- lm(y~1)
+#'
+#' res$AICc0 # corrected AIC for the null model
+#' extractAIC(lm2) # classical AIC
+#' res$AICc0-2*(1*(1+1))/(nrow(x)-1-1) # the same
+#'
+#' @export
+
+
+"ortho.AIC" <-
+    function(Y, X, ord.var = FALSE) {
+        # Fast Forward Selection AIC if X is orthonormal (XtX=I)
+        # return a vector of AICc
+        # if ord.var=TRUE, a list containing also order of variables is returned
+        if (sum(apply(as.matrix(apply(X, 2, mean)), 1, function(x)
+            identical(all.equal(x, 0), TRUE))) != ncol(X))
+            stop("X variables are not centered")
+        X <- sweep(X, 2, sqrt(colSums(X^2)), "/")
+         if (!(sum(identical(all.equal(
+             sum(crossprod(as.matrix(X)) - diag(ncol(X))), 0
+         ), TRUE))))
+             stop("X variables are not orthonormalized")
+         
+        f1 <- function(resp, X) {
+            R2 <- t(as.matrix(X)) %*% as.matrix(Y)
+            R2 <- t(R2) %*% R2
+            return(sum(diag(R2)))
+        }
+        
+        Y <- scale(Y, scale = FALSE)
+        Y <- as.matrix(Y)
+        R2 <- apply(X, 2, f1, resp = Y)
+        SSTot <- sum(diag(t(Y) %*% Y))
+        RSS <- SSTot - R2
+        ordre <- order(RSS)
+        RSS <- sort(RSS)
+        R2 <- R2[ordre]
+        RSScum <- cumsum(c(RSS[1], -R2[-1]))
+        RSScum <- c(SSTot, RSScum)
+        # By default, Y is centered
+        # K is the number of othogonal vectors + 1 (for intercept)
+        
+        K <- (1 + (0:ncol(X)))
+        AICtri <-
+            nrow(X) * log(ifelse(RSScum <= 0, NA, RSScum / nrow(X))) + 2 * K
+        correct <- 2 * (K * (K + 1)) / (nrow(X) - K - 1)
+        correct <- ifelse(is.finite(correct) & (correct > 0), correct, NA)
+        AICc <- AICtri + correct
+        if (ord.var) {
+            AICc <-
+                list(
+                    AICc = AICc[-1],
+                    AICc0 = AICc[1],
+                    ord = ordre,
+                    R2 = cumsum(R2 / SSTot)
+                )
+        }
+        if (!ord.var)
+            AICc <- AICc[-1]
+        return(AICc)
+    }
---
+++ R/test.W.R2.R
@@ -0,0 +1,144 @@
+#' Function to compute eigenvectors of spatial weighting matrices
+#' 
+#' This function is a user-friendly way to compute eigenvectors for various 
+#' possible definitions of spatial weighting matrices (W matrices). It uses the 
+#' function \code{scores.listw} to build the MEM variables on the basis of the 
+#' W matrix provided. Contrary to \code{\link{listw.select}}, \code{test.W.R2} 
+#' does not test the significance of the W matrix nor select the best subset of
+#' spatial predictors within it; it simply generates and returns the complete set 
+#' of MEM variables positively, negatively or both positively and negatively
+#' spatially autocorrelated, depending on the specification of the argument 
+#' \code{MEM.autocor}. 
+#' 
+#' @details This functions allows to test one binary spatial weighting matrix 
+#'   (if only Y and nb are provided). It allows also to test a weighting 
+#'   function based on distances (if f is provided) and a weighting function 
+#'   with different values of parameters if other arguments of \code{f} are 
+#'   provided. 
+#'   \code{test.W.R2} is the new version of the old \code{test.W} function. The
+#'   latter also generated the MEM variables of the W matrix provided (argument
+#'   \code{nb}), but it additionally selected a best W matrix based on an 
+#'   AIC criterion (Dray et al. 2006). This function presented two major issues:
+#'   it was used without control of the global type I error rate (no correction
+#'   of the significance threshold value of the W matrices), and was used in
+#'   many studies to select a best subset of spatial predictors, while the method
+#'   was not designed for this purpose and therefore led to highly inflated type
+#'   I error rates (see Bauman et al. 2018a for details). The function 
+#'   \code{test.W} should therefore not be used anymore. Instead, the user can
+#'   use \code{test.W.R2} to generate the MEM variables according to different W
+#'   matrices (but see warnings below), or can use the combination of 
+#'   \code{\link{listw.candidates}} and \code{\link{listw.select}} which is the best
+#'   user-friendly way to both select a W matrix and a subset of spatial predictors
+#'   within it while keeping a correct type I error rate (Bauman et al. 2018b).
+#'   Note that if a significance test is performed on a W matrix, the significance 
+#'   threshold value must be corrected to consider the total number of W matrices 
+#'   tested (Bauman et al. 2018b). For instance, if the user buids W matrices using 
+#'   a Gabriel's graph and a weighting function for which five parametre values are 
+#'   tested, then a list of five W matrices will be returned by \code{test.W.R2}, 
+#'   and the significance of each of the five W matrices should be tested at a 
+#'   corrected significance threshold for five tests to avoid any type I error rate 
+#'   inflation. Since \code{test.W.R2} only generates the MEM variables, the user must 
+#'   test himself the significance of the global model (whole set of MEM
+#'   variables generated) at the corrected significance threshold value, and
+#'   then proceed to an eigenvector selection within the chosen W matrix (see 
+#'   Bauman et al. 2018a for a discussion on which spatial eigenvector selection
+#'   method to use in which situation).
+#'   The function \code{\link{listw.candidates}} builds a list of W matrices
+#'   the user is interested to compare, and this list can be entered to the
+#'   function \code{\link{listw.select}} which optimises the selection of the 
+#'   W matrix while controling the type I error rate, and then selects the best
+#'   spatial predictor subset within the optimal W matrix.
+#'   
+#' @param nb An object of the class \code{nb} created by functions of the 
+#'   \code{spdep} package
+#' @param xy Coordinates of the samples, this argument is optional and is 
+#'   required only if the argument \code{f} is not null.
+#' @param MEM.autocor A string indicating if all MEM must be returned or only 
+#'   those corresponding to positive or negative autocorrelation; the default is 
+#'   \code{positive}
+#' @param style Coding scheme style (see \code{nb2listw} of the \code{spdep}
+#'   package). Can take values 'W', 'B', 'C', 'U', 'minmax', and 'S'; default is
+#'   'B'
+#' @param f A function of the distance that can be used as a weighting spatial 
+#'   function. This argument is optional
+#' @param \dots Others arguments for the function \code{f}. It defines the range
+#'   of parameters which will be tested
+#' @return A list with the following elements: \item{param }{Only if \code{f} is
+#' different than \code{NULL} and if more than one parametre value was used in
+#' the function; A table of the different combinations of parametres for which a 
+#' W matrix was built.} \item{MEM }{A list containing the results of 
+#'   \code{scores.listw} for all the combinations of \code{param}.}
+#' @author Stephane Dray and Bauman David \email{stephane.dray@@univ-lyon1.fr}, 
+#' \email{davbauman@@gmail.com}
+#' @seealso   \code{\link{scores.listw}}, \code{\link{listw.candidates}}, 
+#' \code{\link{listw.select}}
+#' @references Dray S., Legendre P. and Peres-Neto P. R. (2006) Spatial 
+#'   modeling: a comprehensive framework for principal coordinate analysis of 
+#'   neighbor matrices (PCNM). Ecological Modelling, 196, 483--493
+#'   
+#' Bauman D., Drouet T., Dray S. and Vleminckx J. (2018a) Disentangling good from bad 
+#' practices in the selection of spatial or phylogenetic eigenvectors. Ecography, 41,
+#' 1--12
+#' 
+#' Bauman D., Fortin M-J, Drouet T. and Dray S. (2018b) Optimizing the choice of 
+#' a spatial weighting matrix in eigenvector-based methods. Ecology
+#' 
+#' @keywords spatial
+#' @examples
+#' 
+#' if(require(ade4) & require(spdep)){
+#' 
+#' data(oribatid) 
+#' 
+#' # Generate a binary spatial weighting matrix
+#' nbtri <- tri2nb(as.matrix(oribatid$xy))
+#' tri.res <- test.W.R2(nbtri)
+#' 
+#' maxi <- max(unlist(nbdists(nbtri, as.matrix(oribatid$xy))))
+#' 
+#' # Generates a simple spatial weighting function of the distance
+#' f1 <- function(x) {1-(x)/(maxi)}
+#' tri.f1 <- test.W.R2(nbtri, f = f1, xy = as.matrix(oribatid$xy))
+#' 
+#' # Generates a spatial weighting function with various values of one parametre
+#' f2 <- function(x, dmax, y) {1-(x^y)/(dmax)^y}
+#' tri.f2 <- test.W.R2(nbtri, f = f2, y = 2:10, dmax = maxi, xy = as.matrix(oribatid$xy))
+#' 
+#' }
+#' 
+#' @importFrom spdep nb2listw
+#' @export
+
+"test.W.R2" <- function(nb, xy, style = "B", MEM.autocor = c("positive", "negative",
+                                                              "all"), f = NULL, ...) 
+{
+  mycall <- pairlist(...)   
+  res <- list()  
+  MEM.autocor <- match.arg(MEM.autocor)
+  control <- FALSE
+  
+  if (!(is.null(f))) {
+    nbdist <- nbdists(nb, as.matrix(xy))
+    if (!(is.null(mycall))) {   
+      param <- expand.grid(as.list(mycall))
+      m1 <- match(names(param), names(formals(f)))
+      for (i in 1:nrow(param)) {
+        formals(f)[m1] <- unclass(param[i, ])
+        res[[i]] <- scores.listw(nb2listw(nb, style = style, 
+                                          glist = lapply(nbdist, f), zero.policy = TRUE),
+                                 MEM.autocor = MEM.autocor)
+        if (i > 1) control <- TRUE
+      }
+    }
+    else {   
+      res[[1]] <- scores.listw(nb2listw(nb, style = style, glist = lapply(nbdist, f)), 
+                               MEM.autocor = MEM.autocor)
+    }
+  }
+  else {   
+    res[[1]] <- scores.listw(nb2listw(nb, style = style), MEM.autocor = MEM.autocor)
+  }
+  
+  if (control == TRUE) list(param = param, MEM = res)
+  else list(MEM = res[[1]])
+}
