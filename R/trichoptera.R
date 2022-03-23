40b059659401ba676267a70c1ce6e2c784f0d535
Stéphane Dray <stephane.dray@univ-lyon1.fr>
2022-03-23 09:24:42 +0100

    Update and recompile the Roxygen documentation

--- R/trichoptera.R
+++ R/trichoptera.R
@@ -1,11 +1,33 @@
 #' Trichoptera data set
 #'
-#' A dataset containing the abundances of 56 species in 220 sites
+#' A dataset containing the abundances of 56 Trichoptera species captured in 1984  
+#' during 100 consecutive days in 22 emergence traps (sites) along the outflow stream  
+#' of Lake Cromwell (45°59'13" N, 73°59'47" W) on the territory of Station de
+#' Biologie des Laurentides (Université; de Montéal).
 #'
+#' The original daily data collected at each site were pooled into 10 survey periods
+#' for the study of Legendre et al. (2010) in order to reduce the very high proportion 
+#' of zeros in the original data matrix. The 22 traps form a regular transect, with
+#' geographic positions 1 to 22. 
+#'
+#' @format A data frame with (10 x 22) = 220 rows and 58 variables (Site, Date, 
+#' and 56 species).
+#'
+#' Row order in the data file: the 22 traps (sites) are nested within the 
+#' 10 survey periods. 
+#' 
+#' @author Data provided by Prof. Pierre-Paul Harper, Département de sciences 
+#' biologiques, Université de Montréal.
+#'
+#' @references 
+#' Legendre, P., M. De Caceres & D. Borcard. 2010. Community surveys through
+#' space and time to assess environmental changes: testing space-time
+#' interaction in the absence of replication. Ecology 91: 262-272.
 #'
 #' @docType data
 #' @keywords datasets
 #' @name trichoptera
 #' @usage data(trichoptera)
-#' @format A data frame with 220 rows and 58 variables (Site, Date, and 56 species)
 NULL
+
+
